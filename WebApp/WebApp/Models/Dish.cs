fe1dc6557f5b41a63fedc16cbd50277acf9c6fd0
Elena Yanushevskaya <Elena Yanushevskaya@ANZELIKA>
2016-06-15 12:40:36 +0300

    my first

---
+++ WebApp/WebApp/Models/Dish.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class Dish
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
+        public string Ingredients { get; set; }
+
+        [Required]
+        public int Weight { get; set; }
+
+        public virtual ICollection<Menu> Menus { get; set; }
+
+    }
+}
